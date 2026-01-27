05ab85c391aae63f544844605e683d3ff5cdb822
Shamim-Faqer <219502059+Shamim-Faqer@users.noreply.github.com>
2026-01-27 09:01:18 +0600

    task-section-ses

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-base-100 shadow-sm flex flex-col md:flex-row">
   <div className="flex-1">
     <a className="btn btn-ghost text-xl">CS — Ticket System</a>
   </div>
-  <div className="flex-none hidden md:flex">
-    <ul className="menu menu-horizontal px-1">
+  <div className="flex w-full md:w-auto mt-2 md:mt-0 ">
+    <ul className="menu menu-horizontal px-1 flex-wrap justify-start md:justify-end w-full">
       <li><a>Home</a></li>
       <li><a>FAQ</a></li>
       <li><a>Changelog</a></li>
       <li><a>Blog</a></li>
       <li><a>Download</a></li>
       <li><a>Contact</a></li>
-      <li><a>New Ticket</a></li>
+      <button className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] p-1 rounded font-bold text-white' >+ New Ticket</button>
     </ul>
   </div>
 </div>
--- src/components/TaskGrid.jsx
+++ src/components/TaskGrid.jsx
@@ -2,24 +2,15 @@ import React from 'react'
 
 const TaskGrid = ({cards}) => {
   return (
-    
-    <div>
-      <h2 className="text-xl font-bold mb-4">Task Status</h2>
+    <div className='bg-gray-200 rounded-xl shadow-xl p-6 m-6'>
+      <div className='space-y-4'>
+        <h2 className='text-xl font-bold mb-4'>Task Status</h2>
+        <p className='p-4 bg-base-200 rounded-xl'>Select a Ticket to add to task Status</p>
+      </div>
 
-      <div className="space-y-4">
-
-        <div className="p-4 bg-base-200 rounded-xl">
-          ✅ Done 
-        </div>
-
-        <div className="p-4 bg-base-200 rounded-xl">
-          ⏳ In Progress
-        </div>
-
-        <div className="p-4 bg-base-200 rounded-xl">
-          ❌ Open 
-        </div>
-
+      <div className='space-y-4'>
+        <h2 className='text-xl font-bold mb-4'>Resolved Tasks</h2>
+        <p className='p-4 bg-base-200 rounded-xl'>Select a Ticket to add to Resolved Tasks</p>
       </div>
     </div>
   )
