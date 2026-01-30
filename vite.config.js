4b84e49c348d1a0c5ebe3ae6c6379bfe12e8e978
Shamim-Faqer <219502059+Shamim-Faqer@users.noreply.github.com>
2026-01-30 09:58:43 +0600

    deploy ready

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+
 const isGHPages = process.env.DEPLOY_ENV === "GH_PAGES";
 
 export default defineConfig({
