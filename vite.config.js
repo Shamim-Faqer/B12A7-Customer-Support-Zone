82922503f1e8e164e33eb7fa3a2b11ba4265267a
Shamim-Faqer <219502059+Shamim-Faqer@users.noreply.github.com>
2026-01-30 09:53:01 +0600

    final2

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-
-const isGithubPages = process.env.DEPLOY_ENV === "GH_PAGES";
+const isGHPages = process.env.DEPLOY_ENV === "GH_PAGES";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: isGithubPages ? "/B12A7-Customer-Support-Zone/" : "/",
+  base: isGHPages ? "/B12A7-Customer-Support-Zone/" : "/",
 });
